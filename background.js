7143a9ecb504448098c4d2f4db74b3dfbfcccac3
Pei Hsuan Hung <afcidk@gmail.com>
2020-03-21 23:01:33 +0800

    Fix

--- background.js
+++ background.js
@@ -1,26 +1,22 @@
 //show startup.html when first installed
 chrome.runtime.onInstalled.addListener(function (object){
     if(object.reason === "install"){
-        chrome.tabs.create({url: "./startup.html"}, function (tab){
-            console.log("opened new tab!");
-        });
+        chrome.tabs.create({url: "./startup.html"});
     }
 });
 
 //onClick icon, run contentScript
 chrome.browserAction.onClicked.addListener(function (tab){
-	console.log("wil execute");
     chrome.tabs.executeScript(null, {
 		file: "contentScript.js"
     }, function() {
         if(chrome.runtime.lastError){
-            console.log(chrome.runtime.lastError);
-            //printErrorMessage();
+            printErrorMessage();
         }
     });
 });
 
 function printErrorMessage(){
-    chrome.notifications.create({type: "basic", iconUrl: "./icon64.png", title: "Fail", message: "錯誤/找不到頁面，正確網址應該以          https://www.facebook.com/groups/ 或是 https://www.facebook.com/messages/t/ 開頭喔^^"});
+    chrome.notifications.create({type: "basic", iconUrl: "./icon64.png", title: "Fail", message: "錯誤/找不到頁面"});
     
 }
--- contentScript.js
+++ contentScript.js
@@ -1,34 +1,55 @@
 var path = window.location.pathname;
-var match_str = ["/groups/([0-9]*?)/"];
-var add_str = ["members"];
+var match_str = ["\/groups\/([0-9]*)?(\/members)?(\/friends)?"];
 
 match_str.forEach(function(value, index){
 	mat = path.match(value);
 	if(mat != null){
-		console.log(path)
-		if(mat[0] === path){
-			window.location.replace(path+"members/");
-			path = window.location.pathname;
-		}
-		getPage(null);
+        let res = mat[0];
+        if (res[res.length-1] == "/") res = res.slice(0, res.length-1)
+        if (!res.includes("members")) {
+			window.location.replace(res+"/members/friends");
+        }
+        else if(!res.includes("friends")) {
+			window.location.replace(res+"/friends");
+        }
+
+        scrollToBottom();
 		return ;
 	}
 });
 
+// Ugly QQ
+function scrollToBottom() {
+    var scrollInterval = null;
+    var prevH = 0;
+    window.scrollBy(0, 99999);
+    scrollInterval = setInterval(compare, 800);
+
+    function compare() {
+        function updatePrevH() {
+            if (prevH == document.body.scrollHeight) {
+                clearInterval(scrollInterval); 
+                getPage(null);
+            }
+            prevH = document.body.scrollHeight;
+        }
+
+        window.scrollBy(0, 99999);
+        setTimeout(updatePrevH, 500);
+    }
+}
+
 function getPage(page){
-	var arr = document.getElementsByClassName("fsl fwb fcb");
+    var arr = document.querySelectorAll('div.ofv0k9yr.cwj9ozl2 > div > div a.oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.oo9gr5id.gpro0wi8.lrazzd5p');
 	var names = [];
-	console.log(arr.length);
 	for(i=0; i<arr.length; i++){
-		var name = arr[i].innerText;
-		if(($.inArray(name, names))==-1){
-			names.push(name);
-		}
+        names.push(arr[i].innerText);
 	}
 
 	var output_string = "";
 	names.forEach(function(value){
-		output_string += "@"+value+" ";
+        if (value.length)
+            output_string += "@"+value+" ";
 	});
 	cpyText(output_string);
 }
@@ -43,4 +64,6 @@ function cpyText(text){
 	dum.select();
 	document.execCommand("copy");
 	document.body.removeChild(dum);
+
+    alert("OK!");
 }
